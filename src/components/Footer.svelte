e5bda44b5c9b685f4ab342c268bdebd355b7a731
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2021-12-14 21:26:59 +0100

    Path fixed and better img formats

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="container mx-auto py-5 rounded border-5 flex items-center content-center  place-content-center border-gray-600 border-opacity-15 text-gray-300 font-bold bg-black bg-opacity-90">
 <p class="text-2xl">Built with:</p>
 
-<a href="https://svelte.dev/"><img class="min-w-15 max-w-30 md:w-15" alt="svelte icon" src="svelte-icon.png"></a>
+<a href="https://svelte.dev/"><img class="min-w-15 max-w-30 md:w-15" alt="svelte icon" src="public/svelte-icon.png"></a>
 
 <span class="font-bold text-2xl">and ❤</span>
 
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header class="grid grid-cols-2 justify-items-center md:items-center items-stretch select-none py-4">
-    <img src="icon.png" class="w-50 sm:w-25 lg:w-50 col-start place-self-center dark:bg-gray-200 dark:rounded-full py-3 px-6"  alt="PiterDev Logo">
+    <img src="public/icon.png" class="w-50 sm:w-25 lg:w-50 col-start place-self-center dark:bg-gray-200 dark:rounded-full py-3 px-6"  alt="PiterDev Logo">
 
 <DarkModeBtn />
 
